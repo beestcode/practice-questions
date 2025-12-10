5b07894fefd9c51512973310eaf2d67173761264
beestcode <153935496+beestcode@users.noreply.github.com>
2025-12-10 15:23:56 +0500

    fizzbuzz using ternary

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 //FIZZBUZZ
 
-function fizzbuzz(){
-    for (let i=1; i<=100; i++){
-        if (i % 3===0 && i % 5===0){
-            console.log("fizzbuzz")
-        }
-        else if (i % 3===0) {
-            console.log("fizz")
-        }
-        else if (i % 5===0) {
-            console.log("buzz")
-        }
-        else {
-            console.log(i)
-        }
-    }
+for (let i=1; i<=100; i++) {
+    console.log((i % 3 === 0 ? 'Fizz' : '') + (i % 5 === 0 ? 'Buzz' : '') || i);
 }
-fizzbuzz()
