f21c986e716aa910902017512d23a0518b6a6c50
beestcode <153935496+beestcode@users.noreply.github.com>
2025-12-10 14:11:05 +0500

    convert  minutes to hours and minutes

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-function ab (){
-    let arr = [8,7,6,4,9,1,2,0,-1,11];
-    let large = arr[0];
-    let small = arr[0]; 
-    for (let i of arr){
-        if ( i >= large ){
-            large = i
-        }
-        if ( i <= small ){
-            small = i 
-        }    
-    }
-    console.log(`Largest number is: ${large}`);
-    console.log(`Smallest number is: ${small}`);
-}
-ab();
+//convert minutes to hours and minutes
 
+convert = (minutes)=> {
+    let hours = Math.floor(minutes / 60);
+    let remainingminutes = minutes % 60
+
+    console.log(`${hours} hours and ${remainingminutes} minutes`);
+}
+
+convert(145)
