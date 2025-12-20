d36e9aa21476db4f9bca0ddd1df38e5eee17fe1f
beestcode <153935496+beestcode@users.noreply.github.com>
2025-12-21 01:40:41 +0500

    capitalize using map

--- String/capitalize_frst_ltr.js
+++ String/capitalize_frst_ltr.js
@@ -1,14 +1,24 @@
-function capitalize(){  
-    let letters = ("hey capitalize first letter of each one word !");
-    let words = letters.split(" ");
-    let capital = []
-    
-    for (i=0; i < words.length; i++){
-        capital.push(words[i].charAt(0).toUpperCase() + words[i].slice(1))
-    }
-    let result = capital.join(" ");
-    console.log(result);
-    
-}
+// Capitalize the first letter of each word in a string
 
-capitalize();
+// function capitalize(){  
+//     let letters = ("hey capitalize first letter of each one word !");
+//     let words = letters.split(" ");
+//     let capital = []
+    
+//     for (i=0; i < words.length; i++){
+//         capital.push(words[i][0].toUpperCase() + words[i].slice(1))
+//     }
+//     let result = capital.join(" ");
+//     console.log(result);
+    
+// }
+
+// capitalize();
+
+
+// capitalizeing using map method
+
+let sentence = "capitalize us using map method";
+
+let result = sentence.split(" ").map(words => words[0].toUpperCase() + words.slice(1)).join(" ");
+console.log(result)
