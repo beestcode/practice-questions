b857acd092016d10eba778e223832fef1008770c
beestcode <153935496+beestcode@users.noreply.github.com>
2026-01-16 01:25:22 +0500

    feat: add function to replace words dynamically without hardcoding

--- String/changeoccurences.js
+++ String/changeoccurences.js
@@ -6,3 +6,12 @@ function change(sentence){
     console.log(replaced)
 }
 change("the ball was so wet")
+
+
+//without hardcoding the words
+const change2 = function(sentence,oldword,newword){
+    let replaced = sentence.replaceAll(oldword,newword);
+    
+    console.log(replaced)
+}
+change2("the cat is so big", "cat","bat")
