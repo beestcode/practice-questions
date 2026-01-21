3731d13f8fb77d46fe74cbf836534e5fbb314e5c
beestcode <153935496+beestcode@users.noreply.github.com>
2026-01-21 10:21:38 +0500

    word based truncate char based truncate

--- String/truncatethestring.js
+++ String/truncatethestring.js
@@ -1,6 +1,16 @@
+//word based
 function truncate(val, limit){
     if(val.length > limit){
         console.log(val.split(" ").slice(0,3).join(" ") + "...")
     }
 }
 truncate("hey i was just testing if this works", 10)
+
+//char based
+function truncate1(val, limit){
+    if(val.length >= limit){
+        return val.slice(0,limit) + "...";
+    }
+    return val
+}
+console.log(truncate1("hey i was just testing if this works", 10))
