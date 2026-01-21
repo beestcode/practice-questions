d651983a65e7353505fa4b8763d6695511bf8f02
beestcode <153935496+beestcode@users.noreply.github.com>
2026-01-21 10:22:14 +0500

    fix: update truncate function to accept dynamic limit for word count

--- String/truncatethestring.js
+++ String/truncatethestring.js
@@ -1,10 +1,10 @@
 //word based
 function truncate(val, limit){
     if(val.length > limit){
-        console.log(val.split(" ").slice(0,3).join(" ") + "...")
+        console.log(val.split(" ").slice(0,limit).join(" ") + "...")
     }
 }
-truncate("hey i was just testing if this works", 10)
+truncate("hey i was just testing if this works", 3)
 
 //char based
 function truncate1(val, limit){
