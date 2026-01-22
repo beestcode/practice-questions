121981303c8277ec1f0612bd04af36d02c89dc7d
beestcode <153935496+beestcode@users.noreply.github.com>
2026-01-22 09:05:07 +0500

    feat: add duplicate1 function to handle removal of consecutive duplicate characters in a sentence

--- String/remove_duplicate_chr.js
+++ String/remove_duplicate_chr.js
@@ -9,3 +9,15 @@ function duplicate(val){
     console.log(result)
 }
 duplicate("bbbaaaaattt")
+
+//for multiple words
+function duplicate1(sentence) {
+    let result = "";
+    for (let char of sentence) {
+        if (result === "" || char !== result[result.length - 1]) {
+            result += char;
+        }
+    }
+    console.log(result);
+}
+duplicate1("bbbaaattt aaanndddd bbbbaall");
