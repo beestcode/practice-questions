d755cf3d7e31b55327f173218c9565f3b7a19e25
beestcode <153935496+beestcode@users.noreply.github.com>
2026-01-28 15:22:47 +0500

    feat: enhance anagram function to handle case insensitivity and whitespace

--- String/anagram.js
+++ String/anagram.js
@@ -1,6 +1,6 @@
 function anagram(str1, str2){
-    let frst_wrd = str1.split("").sort().join("")
-    let scnd_wrd = str2.split("").sort().join("")
+    let frst_wrd = str1.toLowerCase().replace(/\s/g,"").split("").sort().join("")
+    let scnd_wrd = str2.toLowerCase().replace(/\s/g,"").split("").sort().join("")
     if(frst_wrd === scnd_wrd){
         console.log("they are anagram")
     }
@@ -8,5 +8,7 @@ function anagram(str1, str2){
         console.log("they are not anagram")
     }
 }
-anagram("hello", "world")
-anagram("hello", "olleh")
+anagram("hello", "world");
+anagram("hello", "olleh");
+anagram("Listen", "Silent");
+anagram("Dormitory", "Dirty Room");
